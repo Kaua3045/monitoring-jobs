4f13d3829972f918b32074ce3ee7c4d5dc6de433
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-19 20:01:52 -0300

    fix(outputs): add the getters and setters in link job output

--- src/main/java/com/kaua/monitoring/jobs/readers/outputs/LinkJobOutput.java
+++ src/main/java/com/kaua/monitoring/jobs/readers/outputs/LinkJobOutput.java
@@ -13,4 +13,20 @@ public class LinkJobOutput implements Serializable {
         this.id = id;
         this.url = url;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
