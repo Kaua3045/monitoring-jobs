951022732f374a5a4f2d506f0bf6cfe2e6f1637b
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-19 18:55:21 -0300

    Initial commit

---
+++ src/main/java/com/kaua/monitoring/jobs/MonitoringJobsApplication.java
@@ -0,0 +1,13 @@
+package com.kaua.monitoring.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringJobsApplication.class, args);
+	}
+
+}
