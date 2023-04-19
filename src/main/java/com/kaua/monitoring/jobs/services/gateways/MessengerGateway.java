5d54bf81fa9f0a9eec5a115ddbda4ee8d551bceb
Kauã Pereira <zkauacodes@hotmail.com>
2023-04-19 19:25:39 -0300

    feat(gateway): remove exception in messenger gateway

--- src/main/java/com/kaua/monitoring/jobs/services/gateways/MessengerGateway.java
+++ src/main/java/com/kaua/monitoring/jobs/services/gateways/MessengerGateway.java
@@ -1,8 +1,6 @@
 package com.kaua.monitoring.jobs.services.gateways;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 public interface MessengerGateway {
 
-    void sendMessage(Object message) throws JsonProcessingException;
+    void sendMessage(Object message);
 }
